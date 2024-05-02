8267f928bdc41a6135a7d713ee7f1334bdc7b7a6
AsiRodri <asier.rodsas@educa.jcyl.es>
2024-05-02 10:51:26 +0200

    Con letra y la fecha de hoy

--- GitHub/src/github/Ejer1.java
+++ GitHub/src/github/Ejer1.java
@@ -9,5 +9,7 @@ public static void main(String[] args) {
 	System.out.println(nombre);
 	
 	System.out.println("(Subele mambo pa que mi gata prenda lo motores)(Subele mambo pa que mi gata prenda lo motores)(Subele mambo pa que mi gata prenda lo motores)(Que se prepararen que lo que viene es pa que le den duro!)");
+	
+	System.out.println("Hoy es 2 de mayo de 2024, buenos dias");
 }
 }
