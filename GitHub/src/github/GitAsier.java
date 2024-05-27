2e930044143a8f9aa6a4d884134b1281b1ddf5e1
Usuario <Usuario@192.168.1.135>
2024-05-27 19:58:40 +0200

    Con comentarios y documentado

--- GitHub/src/github/GitAsier.java
+++ GitHub/src/github/GitAsier.java
@@ -1,5 +1,11 @@
 package github;
-
+/**
+ * 
+ * @author Usuario
+ * @version 1.33
+ * @since 1.1
+ * @return 2 cadenas de texto
+ */
 public class GitAsier {
 public static void main(String[] args) {
 	System.out.println("Prueba de casa");
